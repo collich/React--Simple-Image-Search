abd5943d137c5c73082f0b7c3f4dd9af4bf8356b
Collin <collin-chua@hotmail.com>
2023-01-27 11:34:21 +0800

    Search Bar completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,13 @@
-const App = () => {
+import SearchBar from "./SearchBar"
 
+const App = () => {
+    const handleSubmit = (term) => {
+        console.log('Search for', term);
+    }
+
+    return (
+        <SearchBar onSubmit={handleSubmit}/>
+    )
 }
 
 export default App
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,27 @@
-const SearchBar = () => {
+import { useState } from "react"
 
+const SearchBar = ({onSubmit}) => {
+    const [item, setItem] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        onSubmit(item)
+    }
+
+    const handleSearch = (e) => {
+        setItem(e.target.value)
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input placeholder="What are you searching for?"
+                onChange={handleSearch}
+                value={item}></input>
+                <div>{item}</div>
+            </form>
+        </div>
+    )
 }
 
 export default SearchBar
